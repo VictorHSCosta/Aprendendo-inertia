0ae4c19dcb914d84690b10a52f6b8865a3ff5dac
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-02-10 11:54:55 -0300

    Configurando a biblioteca

---
+++ app/javascript/pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+export default function LandingPage() {
+    return(
+        <main className="flex flex-col items-center">
+            <h1>Welcome to the Inertia + Vite Ruby + React Example</h1>
+            <p>
+                This is the landing page for the Inertia + Vite Ruby + React example. Click on the links in the navigation bar to see the other pages.
+            </p>
+        </main>
+    );
+}
